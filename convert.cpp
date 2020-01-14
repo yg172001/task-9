1b2960b83179d91af01fc421520cef8fab01286a
yg172001 <56976456+yg172001@users.noreply.github.com>
2020-01-15 03:18:34 +0530

    Add files via upload

---
+++ convert.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+	int year,day,week;
+ 
+	cout<<"Enter No. of days:";
+	cin>>day;
+	
+	year=day/365;
+	day=day%365;
+	week=day/7;
+	day=day%7;
+ 
+	cout<<"\nYears: "<<year<<"\nWeeks: "<<week<<"\nDays: "<<day;
+	return 0;
+}
